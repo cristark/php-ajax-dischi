ca5e1d42d6835a445808b9b0912a61f9d1df2665
cris <cristiano.tarconi@gmail.com>
2021-03-01 18:51:59 +0100

    basic scss style

--- index_php.php
+++ index_php.php
@@ -8,15 +8,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>Music Database</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
+    <!-- HEADER -->
+    <header>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/7/71/Spotify.png" alt="Logo Spotify">
+    </header>
+
     <!-- MAIN -->
     <main>
-        <?php foreach ($musicDisk as $disk) { ?>
-            <h2> <?php echo $disk["title"] ?></h2>
-            <h3> <?php echo $disk["author"] ?></h3>
-        <?php } ?> 
+        <div class="container">
+            <?php foreach ($musicDisk as $disk) { ?>
+                <div class="album_box">
+                    <img src="<?php echo $disk["poster"] ?>" alt="<?php echo $disk["title"] ?>">
+                    <h2> <?php echo $disk["title"] ?></h2>
+                    <h3> <?php echo $disk["author"] ?></h3>
+                    <div class="specs">
+                        <p><?php echo $disk["year"] ?></p>
+                        <p><?php echo $disk["genre"] ?></p>
+                    </div>
+                </div>
+            <?php } ?> 
+        </div>
     </main>
 
 </body>
